547f7d75c3a3cf37e4a17e42527dcb871ebb8239
Alexis Lucien <alexis.lucien-ext@open-groupe.com>
2021-07-06 16:30:29 +0200

    Utilisation d'un objet + contrôle à la 1ère personne

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import random
 import numpy as np
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
+random_color = lambda: color.colors[random.choice(color.color_names)]
+
+class Voxel(Entity):
+	def __init__(self, position):
+		super().__init__(model='cube', collider='box', texture='white_cube', color=random_color(), position=position)
+	
+	def input(self, key):
+		if not self.hovered:
+			return
+		
+		distance_from_player = distance(player.position, self.position + mouse.normal)
+		if key == 'left mouse up' and distance_from_player < 3:
+			destroy(self) ## supprime le bloc
+		elif key == 'right mouse up' and distance_from_player < 4:
+			Voxel(position=self.position + mouse.normal) ## créer un nouveau bloc
+
 ################################################################################
 
-camera.position = (7.5, 15, -20)
-camera.rotation = (30, 0, 0)
+player = FirstPersonController(position=(5, 10, 5))
 
+# génération du monde
 world = np.ones((16, 16), dtype=int)
+chunk = list()
 for (x, z), value in np.ndenumerate(world):
-	random_color = color.colors[random.choice(color.color_names)]
-	Entity(model='cube', collider='box', texture='white_cube', color=random_color, position=(x, 0, z))
-
-def update():
-	pass
+	chunk.append(Voxel((x, 0, z)))
 
 ################################################################################
 
